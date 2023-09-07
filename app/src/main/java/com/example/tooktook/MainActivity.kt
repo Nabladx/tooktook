367cdff31896033468fc1b866d26efc7db75c16c
andrew <andreykirilichev@gmail.com>
2023-09-07 19:47:35 +0700

    new

--- app/src/main/java/com/example/tooktook/MainActivity.kt
+++ app/src/main/java/com/example/tooktook/MainActivity.kt
@@ -4,19 +4,37 @@ import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.view.ViewParent
 import android.widget.Adapter
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
+import android.widget.ListView
 import android.widget.Spinner
 import android.widget.Toast
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity()
+    //AdapterView.OnItemClickListener
+{
     @SuppressLint("MissingInflatedId")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+
+//        val lisView: ListView = findViewById(R.id.multiple_list_view)
+//        val listOfItem: ArrayList<String> = setMultipleListView()
+//
+//        val arrayAdapter: ArrayAdapter<String> = ArrayAdapter(applicationContext,
+//            android.R.layout.simple_list_item_multiple_choice,
+//            listOfItem)
+//
+//        lisView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
+//        lisView.adapter = arrayAdapter
+//        lisView.onItemClickListener = this
+
 
         val mode = listOf("Practice yourself", "Practice others")
         val autoComplete : AutoCompleteTextView = findViewById(R.id.auto_complete)
@@ -38,4 +56,21 @@ class MainActivity : AppCompatActivity() {
             Toast.makeText(this, "Language: $itemSelected", Toast.LENGTH_SHORT).show()
         }
     }
+
+//    private fun setMultipleListView(): ArrayList<String>{
+//        val arrayList: ArrayList<String> = ArrayList()
+//
+//        for (i in 1..2){
+//            arrayList.add("Application List $i")
+//        }
+//        return arrayList
+//    }
+//
+//    override fun onItemClick(parent: AdapterView<*>?, view: View?, position: Int, id: Long){
+//
+//        val options: String = parent?.getItemAtPosition(position) as String
+//        Toast.makeText(applicationContext, "Clicked By : $options",
+//            Toast.LENGTH_LONG).show()
+//
+//    }
 }
